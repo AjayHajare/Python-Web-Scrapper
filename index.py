b1c3c3a46b047cd4fb7c4ec90a244db2cf44ef1e
AjayHajare <30743629+AjayHajare@users.noreply.github.com>
2021-05-17 15:55:02 +0530

    Defined main() funciton

--- index.py
+++ index.py
@@ -3,16 +3,23 @@ from bs4 import BeautifulSoup
 import validators
 from pymongo import MongoClient
 
-URL = "https://flinkhub.com"                    #define the URL
+
 
 client = MongoClient()                          #instablish a connection
 db = client["Crawler"]                          #define the collection
-db.Links.insert_one( {"link":URL} )             #insert root URL in database
 
-req = requests.get(URL)                         #send request to server and server will send 'response HTLML' 
+def main():
+    URL = "https://flinkhub.com"                    #define the URL
+    db.Links.insert_one( {"link":URL} )             #insert root URL in database
 
-parse_content = BeautifulSoup(req.text, 'html5lib')     #parse 'response HTML' using BeautifulSoup
+    req = requests.get(URL)                         #send request to server and server will send 'response HTLML' 
 
-for i in parse_content.find_all('a', href=True):        #extract <a> tag from parse content
-    if validators.url(i['href']):                       #check for valid URL
-        db.Links.insert_one({"link":i['href']})         #if URL is valid insert it into database
+    parse_content = BeautifulSoup(req.text, 'html5lib')     #parse 'response HTML' using BeautifulSoup
+
+    for i in parse_content.find_all('a', href=True):        #extract <a> tag from parse content
+        if validators.url(i['href']):                       #check for valid URL
+            db.Links.insert_one({"link":i['href']})         #if URL is valid insert it into database
+
+
+if __name__ == '__main__' :
+        main()
