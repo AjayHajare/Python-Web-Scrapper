3d95dc7a776d10285bb28c526a9e55165b7bb859
AjayHajare <30743629+AjayHajare@users.noreply.github.com>
2021-06-24 19:57:09 +0530

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -6,9 +6,25 @@ crawler.py     -  file which content user defined functions to carry out scrapin
 database.py    -  file which contest user defined functions to perform database operations
 """
 
+# The url to start with
+root_URL = https://flinkhub.com/
 
-root_URL = "https://www.linkedin.com/"
+# Folder for storing retrieved html pages
+HTML_response
 
+# Maximum link limit that can be added in a database
+MAX_LINK_LIMIT = 5000
+
+# Database Name
 databaseName = "Crawler"
 
+# Table Name which stores data
+tableName = Links
 
+
+# Count of maximum thread that can be executed simultaneously
+threadCount = 5
+
+# Process will stop for specified amount of time after updates of all links i.e. each update cycle
+# Format: in seconds
+SLEEP_INTERVAL = 5
